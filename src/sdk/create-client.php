4d23da5f3725558cf43470c07a7d7d9f132ae6dc
Cristiano <cris.moraes.ds@gmail.com>
2023-06-16 10:05:19 -0300

    add error tests

--- src/sdk/create-client.php
+++ src/sdk/create-client.php
@@ -5,4 +5,4 @@ use OpenPix\PhpSdk\Client;
 require_once __DIR__ . "/../../vendor/autoload.php";
 require_once __DIR__ . "/helpers.php";
 
-return Client::create($argv[1] ?? die("Run script with php " . $argv[0] . " <your app id here>.\n"));
+return Client::create($argv[1] ?? getenv("APP_ID") ?? die("Run script with php " . $argv[0] . " <your app id here>.\n"));
---
+++ src/sdk/invalid-app-id.php
@@ -0,0 +1,4 @@
+<?php
+
+$argv[1] = "INVALID APP ID";
+require_once __DIR__ . "/../customers/getOne.php";
